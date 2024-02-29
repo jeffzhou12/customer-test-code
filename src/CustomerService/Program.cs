d1f9c4fdd79844393add6e0046691e14ec82e9bc
jeff.zhou <jeff.zhou127@outlook.com>
2024-02-29 14:25:02 +0800

    add the test script and report remove the swagger used

--- src/CustomerService/Program.cs
+++ src/CustomerService/Program.cs
@@ -1,6 +1,6 @@
 using CustomerService.Contracts;
 using CustomerService.Services;
-using Microsoft.OpenApi.Models;
+//using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,28 +10,28 @@ builder.Services.AddScoped<ICustomersService, CustomersService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(
-     options =>
-     {
-         options.SwaggerDoc("v1", new OpenApiInfo()
-         {
-             Title = "Customer services",
-             Version = "v1",
-             Description = "The customer data service api",
-         });
-         var path = Path.Combine(AppContext.BaseDirectory, "CustomerService.xml");
-         options.IncludeXmlComments(path, true);
-         options.OrderActionsBy(_ => _.RelativePath);
-});
+//builder.Services.AddSwaggerGen(
+//     options =>
+//     {
+//         options.SwaggerDoc("v1", new OpenApiInfo()
+//         {
+//             Title = "Customer services",
+//             Version = "v1",
+//             Description = "The customer data service api",
+//         });
+//         var path = Path.Combine(AppContext.BaseDirectory, "CustomerService.xml");
+//         options.IncludeXmlComments(path, true);
+//         options.OrderActionsBy(_ => _.RelativePath);
+//});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
